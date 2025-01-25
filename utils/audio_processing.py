60d901685b6f75a7c15815b77966b9a0e69a559b
Pierre <pierre.cayol@gmail.com>
2025-01-25 09:12:24 +0100

    :sparkles: Functionnal chunking with start as end of the previous segment

--- utils/audio_processing.py
+++ utils/audio_processing.py
@@ -14,17 +14,20 @@ def process_audio(file: str) -> Tuple[List[bytes], List[Dict[str, int]]]:
     # Detect non-silent chunks
     nonsilent_ranges = silence.detect_nonsilent(audio, min_silence_len=750, silence_thresh=-40)
     
-    # Adjust the end time of each chunk to the start time of the next chunk
-    for i in range(len(nonsilent_ranges) - 1):
-        nonsilent_ranges[i][1] = nonsilent_ranges[i + 1][0]
-    
-    # Split audio based on adjusted non-silent ranges
-    audio_chunks = [audio[start:end] for start, end in nonsilent_ranges]
+    # Adjust the start time of each chunk to the end time of the previous chunk
+    output_ranges = []
+    for i, (start, end) in enumerate(nonsilent_ranges):
+        tuple_range = (start, end)
+        if i == 0:
+            tuple_range = (0, end)
+        else:
+            tuple_range = (nonsilent_ranges[i-1][1], end)
+        output_ranges.append(tuple_range)
     
     # Convert audio chunks to .wav files and collect timestamps
     wav_chunks: List[bytes] = []
     timestamps: List[Dict[str, int]] = []
-    for i, (start, end) in enumerate(nonsilent_ranges):
+    for i, (start, end) in enumerate(output_ranges):
         chunk = audio[start:end]
         wav_io = io.BytesIO()
         chunk.export(wav_io, format="wav")
@@ -32,16 +35,5 @@ def process_audio(file: str) -> Tuple[List[bytes], List[Dict[str, int]]]:
         timestamps.append({'start': start, 'end': end, 'index': i})
         # Print the length of each chunk in seconds
         print(f"Chunk {i}: Length = {(end - start) / 1000} seconds")
-    
-    # Ensure the last chunk ends at the original end time
-    if len(nonsilent_ranges) > 0:
-        last_start, last_end = nonsilent_ranges[-1]
-        last_chunk = audio[last_start:]
-        wav_io = io.BytesIO()
-        last_chunk.export(wav_io, format="wav")
-        wav_chunks[-1] = wav_io.getvalue()
-        timestamps[-1]['end'] = len(audio)
-        # Print the length of the last chunk in seconds
-        print(f"Chunk {len(nonsilent_ranges) - 1}: Length = {(len(audio) - last_start) / 1000} seconds")
-    
+
     return wav_chunks, timestamps
